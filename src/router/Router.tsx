a2d7f938ec39cb122b79d890482726aae95928bb
Markonius <markonius.dev@gmail.com>
2019-11-12 16:07:21 +0100

    :lipstick: Initializing style

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,30 +5,33 @@ import {
   Route,
   Link
 } from 'react-router-dom';
+import '../App.css';
 
 const Router: React.FC = () => {
   return(
     <BrowserRouter>
-      <header>
-        <nav>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/page2'>Page 2</Link></li>
-            <li><Link to='/page3'>Page 3</Link></li>
-          </ul>
-        </nav>
-      </header>
-      <Switch>
-        <Route exact path='/'>
-          <h1>Page 1</h1>
-        </Route>
-        <Route path='/page2'>
-          <h1>Page 2</h1>
-        </Route>
-        <Route path='/page3'>
-          <h1>Page 3</h1>
-        </Route>
-      </Switch>
+      <div className="App">
+        <header>
+          <nav className="Nav">
+            <ul className="Nav-links">
+              <li><Link to='/' className="Nav-link">Home</Link></li>
+              <li><Link to='/page2' className="Nav-link">Page 2</Link></li>
+              <li><Link to='/page3' className="Nav-link">Page 3</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <Switch>
+          <Route exact path='/'>
+            <h1>Page 1</h1>
+          </Route>
+          <Route path='/page2'>
+            <h1>Page 2</h1>
+          </Route>
+          <Route path='/page3'>
+            <h1>Page 3</h1>
+          </Route>
+        </Switch>
+      </div>
     </BrowserRouter>
   )
 }
